eef52c6d983ed201eb1fabaf10533d35d5f946bb
Rico <jrico8@bloomberg.com>
2022-12-23 10:39:11 -0500

    zig 0.10 and human-readable timestamps

--- src/server_main.zig
+++ src/server_main.zig
@@ -1,5 +1,6 @@
 const builtin = @import("builtin");
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 
 const http = @import("http-common");
 const server = @import("http-server");
@@ -17,12 +18,65 @@ pub const log_level: std.log.Level = switch (builtin.mode) {
 const ServerCallbackError = server.Writer.Error || error {InternalServerError};
 
 const ServerState = struct {
-    allocator: std.mem.Allocator,
+    allocator: Allocator,
+    port: u16,
+
+    const Self = @This();
+
+    fn init(allocator: Allocator, port: u16) !Self
+    {
+        return Self {
+            .allocator = allocator,
+            .port = port,
+        };
+    }
+
+    fn deinit(self: *Self) void
+    {
+        _ = self;
+    }
 };
 
-fn onBadFile(writer: server.Writer, fileName: []const u8, err: anyerror) !void
+const BuildEntry = struct {
+    project: []const u8,
+    name: []const u8,
+    timestamp: u64,
+
+    const Self = @This();
+
+    fn init(path: []const u8, allocator: Allocator) !Self
+    {
+        const basename = std.fs.path.basename(path);
+        var nameSplit = std.mem.split(u8, basename, ".");
+        const project = nameSplit.next() orelse return error.NoSplitFirst;
+        const timestampString = nameSplit.next() orelse return error.NoSplitSecond;
+        const timestamp = try std.fmt.parseUnsigned(u64, timestampString, 10);
+
+        return Self {
+            .project = try allocator.dupe(u8, project),
+            .name = try allocator.dupe(u8, basename),
+            .timestamp = timestamp,
+        };
+    }
+
+    fn greaterThan(_: void, lhs: Self, rhs: Self) bool
+    {
+        return lhs.timestamp > rhs.timestamp;
+    }
+};
+
+fn timestampToString(timestamp: u64, allocator: Allocator) ![]const u8
 {
-    try std.fmt.format(writer, "<p>ERROR <b>{}</b>, BAD FILE <b>{s}</b></p>", .{err, fileName});
+    const epochSeconds = std.time.epoch.EpochSeconds{ .secs = timestamp };
+    const epochDay = epochSeconds.getEpochDay();
+    const daySeconds = epochSeconds.getDaySeconds();
+    const yearDay = epochDay.calculateYearDay();
+    const monthDay = yearDay.calculateMonthDay();
+    const hours = daySeconds.getHoursIntoDay();
+    const minutes = daySeconds.getMinutesIntoHour();
+    const seconds = daySeconds.getSecondsIntoMinute();
+
+    return std.fmt.allocPrint(allocator, "{d:0>4}/{d:0>2}/{d:0>2} {d:0>2}:{d:0>2}:{d:0>2} UTC", .{yearDay.year, @enumToInt(monthDay.month), monthDay.day_index + 1, hours, minutes, seconds});
 }
 
 fn serverCallback(
@@ -30,73 +84,55 @@ fn serverCallback(
     request: server.Request,
     writer: server.Writer) !void
 {
-    const allocator = state.allocator;
+    var arenaAllocator = std.heap.ArenaAllocator.init(state.allocator);
+    defer arenaAllocator.deinit();
+    const allocator = arenaAllocator.allocator();
 
     if (std.mem.eql(u8, request.uri, "/")) {
-        try server.writeCode(writer, ._200);
-        try server.writeEndHeader(writer);
-
-        const BuildEntry = struct {
-            fileName: []const u8,
-            timestamp: u64,
-
-            const Self = @This();
-
-            fn greaterThan(_: void, lhs: Self, rhs: Self) bool
-            {
-                return lhs.timestamp > rhs.timestamp;
-            }
-        };
         var entries = std.ArrayList(BuildEntry).init(allocator);
         defer entries.deinit();
 
-        var dir = try std.fs.cwd().openDir(PATH_BUILDS, .{.iterate = true});
-        defer dir.close();
-        var it = dir.iterate();
-        while (try it.next()) |entry| {
-            if (entry.kind != .Directory) {
+        const cwd = std.fs.cwd();
+        var dirIterable = try cwd.openIterableDir(PATH_BUILDS, .{});
+        defer dirIterable.close();
+        var walker = try dirIterable.walk(allocator);
+        defer walker.deinit();
+        while (try walker.next()) |entry| {
+            if (entry.kind != .File) {
                 continue;
             }
 
-            try std.fmt.format(writer, "<h2>{s}</h2>", .{entry.name});
-            var dir2 = try dir.openDir(entry.name, .{.iterate = true});
-            defer dir2.close();
-            var it2 = dir2.iterate();
+            const e = try BuildEntry.init(entry.path, allocator);
+            try entries.append(e);
+        }
 
-            for (entries.items) |e| {
-                allocator.free(e.fileName);
-            }
-            entries.clearRetainingCapacity();
-
-            while (try it2.next()) |entry2| {
-                if (entry2.kind != .File) {
-                    continue;
+        const SortFunc = struct {
+            fn lessThan(_: void, lhs: BuildEntry, rhs: BuildEntry) bool
+            {
+                const projectOrder = std.mem.order(u8, lhs.project, rhs.project);
+                switch (projectOrder) {
+                    .lt => return true,
+                    .gt => return false,
+                    .eq => {},
                 }
-                var nameSplit = std.mem.split(u8, entry2.name, ".");
-                _ = nameSplit.next() orelse {
-                    try onBadFile(writer, entry2.name, error.NoSplitFirst);
-                    continue;
-                };
-                const timestampString = nameSplit.next() orelse {
-                    try onBadFile(writer, entry2.name, error.NoSplitSecond);
-                    continue;
-                };
-                const timestamp = std.fmt.parseUnsigned(u64, timestampString, 10) catch |err| {
-                    try onBadFile(writer, entry2.name, err);
-                    continue;
-                };
+                return lhs.timestamp > rhs.timestamp;
+            }
+        };
 
-                const buildEntry = BuildEntry {
-                    .fileName = try allocator.dupe(u8, entry2.name),
-                    .timestamp = timestamp,
-                };
-                try entries.append(buildEntry);
+        std.sort.sort(BuildEntry, entries.items, {}, SortFunc.lessThan);
+
+        try server.writeCode(writer, ._200);
+        try server.writeEndHeader(writer);
+
+        var lastProject: []const u8 = "";
+        for (entries.items) |e| {
+            if (!std.mem.eql(u8, lastProject, e.project)) {
+                try std.fmt.format(writer, "<h2>{s}</h2>", .{e.project});
+                lastProject = e.project;
             }
 
-            std.sort.sort(BuildEntry, entries.items, {}, BuildEntry.greaterThan);
-            for (entries.items) |e| {
-                try std.fmt.format(writer, "<p><a href=\"{s}/{s}\">{s}</a></p>", .{entry.name, e.fileName, e.fileName});
-            }
+            const timestampString = try timestampToString(e.timestamp, allocator);
+            try std.fmt.format(writer, "<p><a href=\"{s}/{s}\"><em>[{s}]</em>&nbsp;&nbsp;&nbsp;&nbsp;{s}</a></p>", .{e.project, e.name, timestampString, e.name});
         }
     } else {
         try server.serveStatic(writer, request.uri, PATH_BUILDS, state.allocator);
@@ -117,6 +153,29 @@ fn serverCallbackWrapper(
     };
 }
 
+fn httpRedirectCallback(_: void, request: server.Request, writer: server.Writer) !void
+{
+    // TODO we don't have an allocator... but it's ok, I guess
+    var buf: [2048]u8 = undefined;
+    const host = http.getHeader(request, "Host") orelse return error.NoHost;
+    const redirectUrl = try std.fmt.bufPrint(&buf, "https://{s}{s}", .{host, request.uriFull});
+
+    try server.writeRedirectResponse(writer, redirectUrl);
+}
+
+fn httpRedirectEntrypoint(allocator: std.mem.Allocator) !void
+{
+    var s = try server.Server(void).init(httpRedirectCallback, {}, null, allocator);
+    const port = 80;
+
+    std.log.info("Listening on {s}:{} (HTTP -> HTTPS redirect)", .{SERVER_IP, port});
+    s.listen(SERVER_IP, port) catch |err| {
+        std.log.err("server listen error {}", .{err});
+        return err;
+    };
+    s.stop();
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
@@ -128,8 +187,8 @@ pub fn main() !void {
 
     const args = try std.process.argsAlloc(allocator);
     defer allocator.free(args);
-    if (args.len != 4) {
-        std.log.err("Expected arguments: port https-chain-path https-key-path", .{});
+    if (args.len < 2) {
+        std.log.err("Expected arguments: port [<https-chain-path> <https-key-path>]", .{});
         return error.BadArgs;
     }
 
@@ -138,41 +197,61 @@ pub fn main() !void {
         chainPath: []const u8,
         keyPath: []const u8,
     };
-    var httpsArgs = HttpsArgs {
-        .chainPath = args[2],
-        .keyPath = args[3],
-    };
-
-    var state = ServerState {
-        .allocator = allocator,
-    };
-    var s: server.Server(*ServerState) = undefined;
-    {
-        const cwd = std.fs.cwd();
-        const chainFile = try cwd.openFile(httpsArgs.chainPath, .{});
-        defer chainFile.close();
-        const chainFileData = try chainFile.readToEndAlloc(allocator, 1024 * 1024 * 1024);
-        defer allocator.free(chainFileData);
-
-        const keyFile = try cwd.openFile(httpsArgs.keyPath, .{});
-        defer keyFile.close();
-        const keyFileData = try keyFile.readToEndAlloc(allocator, 1024 * 1024 * 1024);
-        defer allocator.free(keyFileData);
-
-        const httpsOptions = server.HttpsOptions {
-            .certChainFileData = chainFileData,
-            .privateKeyFileData = keyFileData,
+    var httpsArgs: ?HttpsArgs = null;
+    if (args.len > 2) {
+        if (args.len != 4) {
+            std.log.err("Expected arguments: port [<https-chain-path> <https-key-path>]", .{});
+            return error.BadArgs;
+        }
+        httpsArgs = HttpsArgs {
+            .chainPath = args[2],
+            .keyPath = args[3],
         };
-        s = try server.Server(*ServerState).init(
-            serverCallbackWrapper, &state, httpsOptions, allocator
-        );
+    }
+
+    var state = try ServerState.init(allocator, port);
+    defer state.deinit();
+
+    var s: server.Server(*ServerState) = undefined;
+    var httpRedirectThread: ?std.Thread = undefined;
+    {
+        if (httpsArgs) |ha| {
+            const cwd = std.fs.cwd();
+            const chainFile = try cwd.openFile(ha.chainPath, .{});
+            defer chainFile.close();
+            const chainFileData = try chainFile.readToEndAlloc(allocator, 1024 * 1024 * 1024);
+            defer allocator.free(chainFileData);
+
+            const keyFile = try cwd.openFile(ha.keyPath, .{});
+            defer keyFile.close();
+            const keyFileData = try keyFile.readToEndAlloc(allocator, 1024 * 1024 * 1024);
+            defer allocator.free(keyFileData);
+
+            const httpsOptions = server.HttpsOptions {
+                .certChainFileData = chainFileData,
+                .privateKeyFileData = keyFileData,
+            };
+            s = try server.Server(*ServerState).init(
+                serverCallbackWrapper, &state, httpsOptions, allocator
+            );
+            httpRedirectThread = try std.Thread.spawn(.{}, httpRedirectEntrypoint, .{allocator});
+        } else {
+            s = try server.Server(*ServerState).init(
+                serverCallbackWrapper, &state, null, allocator
+            );
+            httpRedirectThread = null;
+        }
     }
     defer s.deinit();
 
-    std.log.info("Listening on {s}:{}", .{SERVER_IP, port});
+    std.log.info("Listening on {s}:{} (HTTPS {})", .{SERVER_IP, port, httpsArgs != null});
     s.listen(SERVER_IP, port) catch |err| {
         std.log.err("server listen error {}", .{err});
         return err;
     };
     s.stop();
+
+    if (httpRedirectThread) |t| {
+        t.detach(); // TODO we don't really care for now
+    }
 }
